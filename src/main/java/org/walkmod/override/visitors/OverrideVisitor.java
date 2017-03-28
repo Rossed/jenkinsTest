353112ead73373c10378f7dfd10c3663903440d6
Ross Edwards <rossedwa@au1.ibm.com>
2017-03-29 10:46:53 +1100

    Files from walkmod-override-plugin added

---
+++ src/main/java/org/walkmod/override/visitors/OverrideVisitor.java
@@ -0,0 +1,178 @@
+
+package org.walkmod.override.visitors;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Type;
+import java.lang.reflect.TypeVariable;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.walkmod.javalang.ast.MethodSymbolData;
+import org.walkmod.javalang.ast.SymbolData;
+import org.walkmod.javalang.ast.body.MethodDeclaration;
+import org.walkmod.javalang.ast.body.ModifierSet;
+import org.walkmod.javalang.ast.body.Parameter;
+import org.walkmod.javalang.ast.expr.AnnotationExpr;
+import org.walkmod.javalang.ast.expr.MarkerAnnotationExpr;
+import org.walkmod.javalang.ast.expr.NameExpr;
+import org.walkmod.javalang.compiler.reflection.ClassInspector;
+import org.walkmod.javalang.compiler.reflection.MethodInspector;
+import org.walkmod.javalang.compiler.symbols.RequiresSemanticAnalysis;
+import org.walkmod.javalang.visitors.VoidVisitorAdapter;
+import org.walkmod.walkers.VisitorContext;
+
+@RequiresSemanticAnalysis
+public class OverrideVisitor extends VoidVisitorAdapter<VisitorContext> {
+
+   private boolean containsOverrideAsAnnotationExpr(MethodDeclaration md) {
+      List<AnnotationExpr> mAnnotations = md.getAnnotations();
+      boolean containsOverride = false;
+      if (mAnnotations != null) {
+         // To avoid a JDK8 bug
+         // (http://stackoverflow.com/questions/26515016/annotations-on-an-overridden-method-are-ignored-in-java-8-propertydescriptor)
+         Iterator<AnnotationExpr> it = mAnnotations.iterator();
+         while (it.hasNext() && !containsOverride) {
+            AnnotationExpr ae = it.next();
+            SymbolData sd = ae.getSymbolData();
+            if (sd != null) {
+               Class<?> clazz = sd.getClazz();
+               containsOverride = clazz.equals(Override.class);
+            }
+            else{
+               containsOverride = "Override".equals(ae.getName().getName());
+            }
+         }
+      }
+      return containsOverride;
+   }
+
+   private boolean containsOverrideInByteCode(MethodDeclaration md) {
+      boolean isAnnotationPresent = true;
+      MethodSymbolData sdata = md.getSymbolData();
+      Method method = sdata.getMethod();
+      try {
+         isAnnotationPresent = method.isAnnotationPresent(Override.class);
+      } catch (Throwable t) {
+         //maybe there are private classes that cannot be accessed by reflection
+         //http://stackoverflow.com/questions/8512207/jetty-guice-illegalaccesserror
+      }
+      return isAnnotationPresent;
+   }
+
+   private boolean containsAnEquivalentParentMethod(MethodDeclaration md) {
+      MethodSymbolData sdata = md.getSymbolData();
+      boolean result = false;
+      if (sdata != null) {
+
+         Method method = sdata.getMethod();
+
+         if (!containsOverrideAsAnnotationExpr(md) && !containsOverrideInByteCode(md)) {
+
+            Class<?> declaringClass = method.getDeclaringClass();
+
+            Class<?> parentClass = declaringClass.getSuperclass();
+
+            if (parentClass != null) {
+
+               // it should be initialized after resolving the method
+               List<Parameter> params = md.getParameters();
+               SymbolData[] args = null;
+               if (params != null) {
+                  args = new SymbolData[params.size()];
+                  int i = 0;
+                  for (Parameter param : params) {
+                     args[i] = param.getType().getSymbolData();
+                     i++;
+                  }
+               } else {
+                  args = new SymbolData[0];
+               }
+
+               List<Class<?>> scopesToCheck = new LinkedList<Class<?>>();
+               scopesToCheck.add(parentClass);
+               Class<?>[] interfaces = declaringClass.getInterfaces();
+               for (int i = 0; i < interfaces.length; i++) {
+                  scopesToCheck.add(interfaces[i]);
+               }
+               Iterator<Class<?>> it = scopesToCheck.iterator();
+               Method foundMethod = null;
+               while (it.hasNext() && foundMethod == null) {
+
+                  Class<?> clazzToAnalyze = it.next();
+
+                  foundMethod = MethodInspector.findMethod(clazzToAnalyze, args, md.getName());
+                  if (foundMethod != null) {
+                     if (!foundMethod.getDeclaringClass().isAssignableFrom(clazzToAnalyze)) {
+                        foundMethod = null;
+                     } else {
+                        List<Type> types = ClassInspector.getInterfaceOrSuperclassImplementations(declaringClass,
+                              clazzToAnalyze);
+                        Class<?> implementation = null;
+                        if (types != null && !types.isEmpty()) {
+                           if (types.get(0) instanceof Class) {
+                              implementation = (Class<?>) types.get(0);
+                           }
+                        }
+
+                        Type[] parameterTypes = foundMethod.getGenericParameterTypes();
+                        int modifiers = foundMethod.getModifiers();
+                        boolean valid = ModifierSet.isPublic(modifiers) || ModifierSet.isProtected(modifiers);
+                        for (int i = 0; i < parameterTypes.length && valid; i++) {
+                           if (parameterTypes[i] instanceof Class) {
+                              valid = (args[i].getClazz().getName().equals(((Class<?>) parameterTypes[i]).getName()));
+                           } else if (parameterTypes[i] instanceof TypeVariable) {
+
+                              TypeVariable<?> tv = (TypeVariable<?>) parameterTypes[i];
+                              if (implementation != null) {
+                                 TypeVariable<?>[] tvs = implementation.getTypeParameters();
+                                 int pos = -1;
+                                 for (int k = 0; k < tvs.length && pos == -1; k++) {
+                                    if (tvs[k].getName().equals(tv.getName())) {
+                                       pos = k;
+                                    }
+                                 }
+                                 if (pos > -1) {
+                                    Type[] bounds = tvs[pos].getBounds();
+                                    for (int k = 0; k < bounds.length && valid; k++) {
+                                       if (bounds[k] instanceof Class<?>) {
+                                          valid = args[i].getClazz().isAssignableFrom((Class) bounds[k]);
+                                       }
+                                    }
+                                 }
+                              }
+                           }
+                        }
+
+                        if (!valid) {
+                           foundMethod = null;
+                        }
+                     }
+                  }
+               }
+               result = foundMethod != null;
+            }
+         }
+      }
+      return result;
+   }
+
+   @Override
+   public void visit(MethodDeclaration md, VisitorContext arg) {
+      if (!ModifierSet.isStatic(md.getModifiers()) && !ModifierSet.isPrivate(md.getModifiers())) {
+
+         if (containsAnEquivalentParentMethod(md)) {
+            List<AnnotationExpr> annotations = md.getAnnotations();
+            if (annotations == null) {
+               annotations = new LinkedList<AnnotationExpr>();
+               md.setAnnotations(annotations);
+            }
+
+            annotations.add(new MarkerAnnotationExpr(new NameExpr("Override")));
+         }
+
+      }
+
+      super.visit(md, arg);
+   }
+}
---
+++ src/test/java/org/walkmod/override/tests/OperationVisitorTest.java
@@ -0,0 +1,294 @@
+package org.walkmod.override.tests;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.walkmod.javalang.ast.CompilationUnit;
+import org.walkmod.javalang.ast.body.BodyDeclaration;
+import org.walkmod.javalang.ast.body.ClassOrInterfaceDeclaration;
+import org.walkmod.javalang.ast.body.MethodDeclaration;
+import org.walkmod.javalang.ast.expr.AnnotationExpr;
+import org.walkmod.javalang.ast.expr.Expression;
+import org.walkmod.javalang.ast.expr.ObjectCreationExpr;
+import org.walkmod.javalang.ast.expr.VariableDeclarationExpr;
+import org.walkmod.javalang.ast.stmt.ExpressionStmt;
+import org.walkmod.javalang.ast.stmt.TypeDeclarationStmt;
+import org.walkmod.javalang.test.SemanticTest;
+import org.walkmod.override.visitors.OverrideVisitor;
+
+public class OperationVisitorTest extends SemanticTest {
+
+	@Test
+	public void testOverrideOnSimpleClass() throws Exception {
+		CompilationUnit cu = compile("public class Foo{ "
+				+ "public String toString(){ return \"\"; }" + " }");
+		OverrideVisitor visitor = new OverrideVisitor();
+		cu.accept(visitor, null);
+		BodyDeclaration method = cu.getTypes().get(0).getMembers().get(0);
+
+		List<AnnotationExpr> annotations = method.getAnnotations();
+		Assert.assertNotNull(annotations);
+
+		AnnotationExpr ann = annotations.get(0);
+
+		Assert.assertEquals("Override", ann.getName().getName());
+	}
+
+	@Test
+	public void testOverrideNotRequired() throws Exception {
+		CompilationUnit cu = compile("public class Foo{ "
+				+ "public boolean equalsTo(Object o){ return false; }" + " }");
+		OverrideVisitor visitor = new OverrideVisitor();
+		cu.accept(visitor, null);
+		BodyDeclaration method = cu.getTypes().get(0).getMembers().get(0);
+
+		List<AnnotationExpr> annotations = method.getAnnotations();
+		Assert.assertNull(annotations);
+	}
+
+	@Test
+	public void testOverrideWithSimpleParameter() throws Exception {
+		CompilationUnit cu = compile("public class Foo{ "
+				+ "public boolean equals(Object o){ return false; }" + " }");
+		OverrideVisitor visitor = new OverrideVisitor();
+		cu.accept(visitor, null);
+		BodyDeclaration method = cu.getTypes().get(0).getMembers().get(0);
+
+		List<AnnotationExpr> annotations = method.getAnnotations();
+		Assert.assertNotNull(annotations);
+
+		AnnotationExpr ann = annotations.get(0);
+
+		Assert.assertEquals("Override", ann.getName().getName());
+	}
+
+	@Test
+	public void testOverrideWithInheritance() throws Exception {
+
+		String fooCode = "public class Foo extends Bar{ "
+				+ "public void doSomething(){}" + " }";
+
+		String barCode = "public class Bar{ " + "public void doSomething(){}"
+				+ " }";
+
+		CompilationUnit cu = compile(fooCode, barCode);
+		OverrideVisitor visitor = new OverrideVisitor();
+		cu.accept(visitor, null);
+		BodyDeclaration method = cu.getTypes().get(0).getMembers().get(0);
+
+		List<AnnotationExpr> annotations = method.getAnnotations();
+		Assert.assertNotNull(annotations);
+
+		AnnotationExpr ann = annotations.get(0);
+
+		Assert.assertEquals("Override", ann.getName().getName());
+	}
+
+	@Test
+	public void testOverrideWithInterfaces() throws Exception {
+
+		String fooCode = "public class Foo implements Bar{ "
+				+ "public void doSomething(){}" + " }";
+
+		String barCode = "public interface Bar{ "
+				+ "public void doSomething();" + " }";
+
+		CompilationUnit cu = compile(fooCode, barCode);
+		OverrideVisitor visitor = new OverrideVisitor();
+		cu.accept(visitor, null);
+		BodyDeclaration method = cu.getTypes().get(0).getMembers().get(0);
+
+		List<AnnotationExpr> annotations = method.getAnnotations();
+		Assert.assertNotNull(annotations);
+
+		AnnotationExpr ann = annotations.get(0);
+
+		Assert.assertEquals("Override", ann.getName().getName());
+	}
+
+	@Test
+	public void testOverrideWithGenerics() throws Exception {
+
+		String fooCode = "import java.util.List; public class Foo implements Bar<List>{ "
+				+ "public void doSomething(List l){}" + " }";
+
+		String barCode = "import java.util.Collection; public interface Bar<T extends Collection>{ "
+				+ "public void doSomething(T c);" + " }";
+
+		CompilationUnit cu = compile(fooCode, barCode);
+		OverrideVisitor visitor = new OverrideVisitor();
+		cu.accept(visitor, null);
+		BodyDeclaration method = cu.getTypes().get(0).getMembers().get(0);
+
+		List<AnnotationExpr> annotations = method.getAnnotations();
+		Assert.assertNotNull(annotations);
+
+		AnnotationExpr ann = annotations.get(0);
+		Assert.assertEquals("Override", ann.getName().getName());
+
+	}
+
+	@Test
+	public void testOverrideWithArrays() throws Exception {
+
+		String fooCode = "import java.util.List; public class Foo implements Bar<List>{ "
+				+ "public void doSomething(List[] l){}" + " }";
+
+		String barCode = "import java.util.Collection; public interface Bar<T extends Collection>{ "
+				+ "public void doSomething(T[] c);" + " }";
+
+		CompilationUnit cu = compile(fooCode, barCode);
+		OverrideVisitor visitor = new OverrideVisitor();
+		cu.accept(visitor, null);
+		BodyDeclaration method = cu.getTypes().get(0).getMembers().get(0);
+
+		List<AnnotationExpr> annotations = method.getAnnotations();
+		Assert.assertNotNull(annotations);
+
+		AnnotationExpr ann = annotations.get(0);
+		Assert.assertEquals("Override", ann.getName().getName());
+
+	}
+
+	@Test
+	public void testOverrideWithArraysDimensions() throws Exception {
+
+		String fooCode = "import java.util.List; public class Foo extends Bar<List>{ "
+				+ "public void doSomething(List[] l){}" + " }";
+
+		String barCode = "import java.util.Collection; public class Bar<T extends Collection>{ "
+				+ "public void doSomething(T[][] c){}" + " }";
+
+		CompilationUnit cu = compile(fooCode, barCode);
+		OverrideVisitor visitor = new OverrideVisitor();
+		cu.accept(visitor, null);
+		BodyDeclaration method = cu.getTypes().get(0).getMembers().get(0);
+
+		List<AnnotationExpr> annotations = method.getAnnotations();
+		Assert.assertNull(annotations);
+	}
+
+	@Test
+	public void testOverrideWithDynamicArgs() throws Exception {
+
+		String fooCode = "import java.util.List; public class Foo extends Bar<List>{ "
+				+ "public void doSomething(List... l){}" + " }";
+
+		String barCode = "import java.util.Collection; public class Bar<T extends Collection>{ "
+				+ "public void doSomething(T... c){}" + " }";
+
+		CompilationUnit cu = compile(fooCode, barCode);
+		OverrideVisitor visitor = new OverrideVisitor();
+		cu.accept(visitor, null);
+		BodyDeclaration method = cu.getTypes().get(0).getMembers().get(0);
+
+		List<AnnotationExpr> annotations = method.getAnnotations();
+		Assert.assertNotNull(annotations);
+
+		AnnotationExpr ann = annotations.get(0);
+		Assert.assertEquals("Override", ann.getName().getName());
+	}
+
+	@Test
+	public void testOverrideOnSimpleInnerClass() throws Exception {
+		CompilationUnit cu = compile("public class Foo{ class Bar {"
+				+ "public String toString(){ return \"\"; }" + " }}");
+		OverrideVisitor visitor = new OverrideVisitor();
+		cu.accept(visitor, null);
+
+		BodyDeclaration innerClass = cu.getTypes().get(0).getMembers().get(0);
+		BodyDeclaration method = ((ClassOrInterfaceDeclaration) innerClass)
+				.getMembers().get(0);
+
+		List<AnnotationExpr> annotations = method.getAnnotations();
+		Assert.assertNotNull(annotations);
+
+		AnnotationExpr ann = annotations.get(0);
+
+		Assert.assertEquals("Override", ann.getName().getName());
+	}
+
+	@Test
+	public void testOverrideOnSimpleTypeDeclarationStmt() throws Exception {
+		CompilationUnit cu = compile("public class Foo{ public void something() {class Bar {"
+				+ "public String toString(){ return \"\"; }" + " }}}");
+		OverrideVisitor visitor = new OverrideVisitor();
+		cu.accept(visitor, null);
+
+		MethodDeclaration firstMethod = (MethodDeclaration) cu.getTypes()
+				.get(0).getMembers().get(0);
+
+		TypeDeclarationStmt stmt = (TypeDeclarationStmt) firstMethod.getBody()
+				.getStmts().get(0);
+
+		BodyDeclaration method = stmt.getTypeDeclaration().getMembers().get(0);
+
+		List<AnnotationExpr> annotations = method.getAnnotations();
+		Assert.assertNotNull(annotations);
+
+		AnnotationExpr ann = annotations.get(0);
+
+		Assert.assertEquals("Override", ann.getName().getName());
+	}
+	
+	@Test
+	public void testCorrectOverrideWithSubtypes() throws Exception{
+	   CompilationUnit cu = compile("public class Foo { public boolean equals(Foo foo){ return false;} }");
+	   OverrideVisitor visitor = new OverrideVisitor();
+      cu.accept(visitor, null);
+
+      MethodDeclaration firstMethod = (MethodDeclaration) cu.getTypes()
+            .get(0).getMembers().get(0);
+      List<AnnotationExpr> annotations = firstMethod.getAnnotations();
+      Assert.assertNull(annotations);
+	}
+	
+	@Test
+	public void testStaticMethods() throws Exception{
+	   CompilationUnit cu = compile("public class Bar extends Foo{ public static void setTestMode(boolean testMode)  {}}","public class Foo { public static void setTestMode(boolean testMode) { }}");
+	   OverrideVisitor visitor = new OverrideVisitor();
+      cu.accept(visitor, null);
+      MethodDeclaration firstMethod = (MethodDeclaration) cu.getTypes()
+            .get(0).getMembers().get(0);
+      List<AnnotationExpr> annotations = firstMethod.getAnnotations();
+      Assert.assertNull(annotations);
+	}
+	
+	@Test
+	public void testIssue3() throws Exception{
+	   CompilationUnit cu = compile("import java.util.Hashtable; final class ThreadLocalMap extends ThreadLocal { public final Object childValue(Object parentValue) { return null; }}");
+	   OverrideVisitor visitor = new OverrideVisitor();
+      cu.accept(visitor, null);
+      MethodDeclaration firstMethod = (MethodDeclaration) cu.getTypes()
+            .get(0).getMembers().get(0);
+      List<AnnotationExpr> annotations = firstMethod.getAnnotations();
+      Assert.assertNull(annotations);
+	}
+	
+	
+	@Test
+	public void testIssue3Generics() throws Exception{
+	   CompilationUnit cu = compile("public class C extends Comparator{ public void compare(java.util.List x){}}", "public class Comparator<T>{ public void compare(T x){} }");
+	   OverrideVisitor visitor = new OverrideVisitor();
+      cu.accept(visitor, null);
+      MethodDeclaration firstMethod = (MethodDeclaration) cu.getTypes()
+            .get(0).getMembers().get(0);
+      List<AnnotationExpr> annotations = firstMethod.getAnnotations();
+      Assert.assertNull(annotations);
+	} 
+	
+	@Test
+	public void testAnonymousClass() throws Exception{
+	   
+	   CompilationUnit cu = compile("public class Foo { public void bye() {} abstract static class Scanner{ void hello(){} } static final class DefaultScanner extends Scanner { void bye() {} } }");
+	   OverrideVisitor visitor = new OverrideVisitor();
+      cu.accept(visitor, null);
+      ClassOrInterfaceDeclaration firstMethod = (ClassOrInterfaceDeclaration) cu.getTypes()
+            .get(0).getMembers().get(2);
+      MethodDeclaration md = (MethodDeclaration) firstMethod.getMembers().get(0);
+      Assert.assertNull(md.getAnnotations());
+	}
+	
+
+}
